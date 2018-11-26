b5f7a23052b2e43a9ab84d50dde83d791589ccd8
David Antunes <dvdantunes@gmail.com>
2018-11-26 13:45:11 -0300

    fix(client-app): Fixing CommonJS syntax to comply to mocha tests

--- client/config.js
+++ client/config.js
@@ -1,5 +1,4 @@
 // Google Maps Api Key
-const GMAPS_API_KEY = "AIzaSyA53AqsfawEZBHOUi2UjHfKvgi37tl7cXA"
+const GMAPS_API_KEY = "AIzaSyA53AqsfawEZBHOUi2UjHfKvgi37tl7cXA";
 
-
-export {GMAPS_API_KEY};
+module.exports = { GMAPS_API_KEY };
