b4c32ff86832b3cabb959d730ecaed23c924aef0
David Antunes <dvdantunes@gmail.com>
2018-11-28 14:05:45 -0300

    refactor(client-app): Improvements on map coordinates selection + Fetch on server api

--- client/components/Map.js
+++ client/components/Map.js
@@ -53,6 +53,7 @@ import React, { Component } from 'react';
 import { Grid, Row, Col } from 'react-bootstrap';
 import GoogleMapReact from 'google-map-react';
 import { GMAPS_API_KEY } from '../config';
+import { postData } from '../helpers/fetch';
 
 
 const SimpleReactComponent = ({ text }) => (
@@ -72,20 +73,180 @@ const SimpleReactComponent = ({ text }) => (
 );
 
 
+/**
+ *
+ *
+ */
 class Map extends Component {
+
+
+  /**
+   *
+   *
+   */
   static defaultProps = {
     center: {
-      lat: 59.95,
-      lng: 30.33
+      lat: 0.0,
+      lng: 0.0
     },
     zoom: 3,
-    zoomControl : false
+    zoomControl : false,
+    disableDoubleClickZoom: true,
+    panControl : false,
+    rotateControl : false,
+    scaleControl : false,
+    mapTypeControl : false,
+    streetViewControl : false,
+    fullscreenControl : false,
+    scrollwheel : false
   };
 
 
-  _onClick = ({x, y, lat, lng, event}) => console.log('_onClick', x, y, lat, lng, event)
+
+  /**
+   *
+   *
+   */
+  constructor(props) {
+    super(props);
+  }
 
 
+
+  /**
+   *
+   * @see https://developers.google.com/maps/documentation/javascript/reference/map#MapOptions
+   * @see https://developers.google.com/maps/documentation/javascript/controls#ControlOptions
+   * @see https://developers.google.com/maps/documentation/javascript/examples/control-positioning
+   */
+  setMapOptions = (maps) => {
+    return {
+      draggableCursor: 'default',
+      gestureHandling : 'auto',
+      zoom : this.props.zoom,
+      zoomControl : this.props.zoomControl,
+      disableDoubleClickZoom: this.props.disableDoubleClickZoom,
+      panControl : this.props.panControl,
+      rotateControl : this.props.rotateControl,
+      scaleControl : this.props.scaleControl,
+      mapTypeControl : this.props.mapTypeControl,
+      streetViewControl : this.props.streetViewControl,
+      fullscreenControl : this.props.fullscreenControl,
+      scrollwheel : this.props.scrollwheel
+    }
+  }
+
+
+
+  /**
+   *
+   *
+   */
+  _onChange = (center, zoom, bounds, marginBounds) => {
+    // this.props.onCenterChange(center);
+    // this.props.onZoomChange(zoom);
+
+    console.log('_onChange', center, bounds, marginBounds);
+  }
+
+
+  /**
+   *
+   *
+   */
+  _onClick = ({x, y, lat, lng, event}) => {
+    console.log('_onClick', x, y, lat, lng, event);
+
+
+    this._fetchTenkiWeatherData(lat, lng);
+  }
+
+
+
+  _fetchTenkiWeatherData = (lat, lng) => {
+
+    const postData = {
+        "latitude": lat,
+        "longitude": lng
+      };
+
+    console.log('fetch', postData, JSON.stringify(postData));
+
+    fetch('http://localhost:4040/api/tenki-forecast', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(postData)
+        })
+        .then(response => {
+
+          console.log('response: ', response.status, response);
+
+          if (response.status !== 200){
+            throw new Error('Bad response: ', response.status);
+          }
+
+          return response.json()
+        })
+        .then(responseBody => {
+          //
+
+          const defaultMessage = "Error retrieving weather data. Please try again";
+
+          // const responseBody = response.json();
+          console.log('success: ', responseBody, JSON.stringify(responseBody));
+
+
+          // Handle expected errors
+
+          if (!responseBody
+              || (responseBody.status == 'error'
+                  && !responseBody.message.includes('No results found')))
+          {
+            return defaultMessage;
+          }
+
+
+          // Click on ocean or bad coordinates
+          if (responseBody.status == 'error'
+              && responseBody.message.includes('No results found'))
+          {
+            return `No weather data was found for selected coordinates.`;
+          }
+
+
+          //
+          const country = responseBody.data.country.name;
+          const capital = responseBody.data.capital.name != 'not-found' ? responseBody.data.capital.name : '';
+          const temperature = responseBody.data.weather.temperature +" ºC"
+          const season = responseBody.data.weather.season;
+
+          return `${country} - ${capital} - ${temperature} - ${season}`;
+        })
+        .then(message => {
+          // message = `<div class='modal-content'></div>`;
+          console.log('message: ', message);
+        })
+        .catch(error => {
+          // Unexpected errors
+          console.error('error: ', error);
+
+          const defaultMessage = "<div class='modal-content'>"
+                               + "  <p class='modal-header'>Error</p>"
+                               + "  <p class='modal-body'>Error retrieving weather data. Please try again</p>"
+                               + "</div>";
+          console.log('messageError: ', defaultMessage);
+        });
+
+  }
+
+
+
+  /**
+   *
+   *
+   */
   _onChildClick = (key, childProps) => {
     // this.props.onCenterChange([childProps.lat, childProps.lng]);
     console.log('_onChildClick', key, childProps)
@@ -93,6 +254,10 @@ class Map extends Component {
 
 
 
+  /**
+   *
+   *
+   */
   render() {
     return (
       <Grid>
@@ -117,10 +282,12 @@ class Map extends Component {
                   key: GMAPS_API_KEY,
                   language: 'en'
                 }}
-                defaultCenter={this.props.center}
+                options={this.setMapOptions}
                 defaultZoom={this.props.zoom}
+                defaultCenter={this.props.center}
                 onClick={this._onClick}
-                _onChildClick={this._onClick}
+                onChildClick={this._onChildClick}
+                onChange={this._onChange}
               >
                 <SimpleReactComponent
                   lat={59.955413}
--- client/config.js
+++ client/config.js
@@ -1,4 +1,5 @@
 // Google Maps Api Key
 const GMAPS_API_KEY = "AIzaSyA53AqsfawEZBHOUi2UjHfKvgi37tl7cXA";
 
+
 module.exports = { GMAPS_API_KEY };
